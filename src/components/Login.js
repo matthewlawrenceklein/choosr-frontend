b79b6421ddd2710cbfb6514f21ceeaf5533ec09f
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-09-14 14:17:52 -0500

    github OAuth added

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ import "firebase/auth";
 
 class Login extends Component {
 
-handleSignIn = () => {
+handleGoogleSignIn = () => {
     var provider = new firebase.auth.GoogleAuthProvider();
         provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
     firebase.auth().signInWithPopup(provider).then(function(result) {
@@ -30,10 +30,34 @@ handleSignIn = () => {
       });
  }
 
+handleGitHubSignIn = () => {
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        const { photoURL, email, displayName } = result.user 
+
+        console.log(displayName)
+        console.log(email)
+        console.log(photoURL)
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });    
+}
     render() {
         return (
             <div>
-                <button onClick={ this.handleSignIn }>login with google</button>
+                <button onClick={ this.handleGoogleSignIn }>login with google</button>
+                <button onClick={ this.handleGitHubSignIn }>login with github</button>
             </div>
         );
     }
