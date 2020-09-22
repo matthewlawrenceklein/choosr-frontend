e88306dd6863be9dc6d611ae271fa41d2a53dcb6
matthewlawrenceklein <mtt.l.kln@gmail.com>
2020-09-21 21:45:09 -0500

    pulling correct data from zomato api call

--- src/components/ChoosieFinish.js
+++ src/components/ChoosieFinish.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import CinemaItem from './choiceItems/CinemaItem'
-import CuisineItem from './choiceItems/CuisineItem';
 import MovieItem from './choiceItems/MovieItem'
-import FinalItem from './choiceItems/FinalItem'
+import FinalCuisineItem from './choiceItems/FinalCuisineItem'
 
 class ChoosieFinish extends Component {
 
@@ -24,21 +23,41 @@ class ChoosieFinish extends Component {
         }
 
     }
+
+    shuffle = (a) => {
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
     handleCuisine = () => {
         const { lat, lng } = this.props.latLon
         const id = parseInt(this.props.choiceSet[0].id)
-        console.log(lat, lng, id)        
+        // add distinction between delivery and pickup, adjust radius search accordingly w variable 
 
-        fetch(`https://developers.zomato.com/api/v2.1/search?count=1&lat=${lat}&lon=${lng}&radius=10000&cuisines=${parseInt(id)}&sort=rating`, {
+        fetch(`https://developers.zomato.com/api/v2.1/search?count=3&lat=${lat}&lon=${lng}&radius=10000&cuisines=${parseInt(id)}&sort=rating`, {
         headers: {
         Accept: "application/json",
         "User-Key": "7b934257aa35e3ba2609e8d3443b4466"
         }})
         .then(resp => resp.json())
         .then(resp => {
-            console.log(resp.restaurants[0].restaurant)
-            console.log(resp.restaurants[0].restaurant.name)
-            console.log(resp.restaurants[0].restaurant.photos_url)
+            const restaurants = this.shuffle(resp.restaurants)
+            // console.log(restaurants[0].restaurant)
+            const { name, location, featured_image, menu_url} = restaurants[0].restaurant
+            console.log(name, location, featured_image, menu_url)
+            this.setState({
+                name : name, 
+                location : location.address,
+                // featured_image : featured_image, 
+                menu : menu_url
+
+            })
         })  
         
     }
@@ -58,11 +77,17 @@ class ChoosieFinish extends Component {
         return (
         <div className='container'>
           <div className='App'>
-              { this.props.setCategory === 'cuisine' ? <FinalItem image={this.props.choiceSet[0].image}/> : null }
+              { this.props.setCategory === 'cuisine' ? <FinalCuisineItem image={this.props.choiceSet[0].image}
+                                                                         name={this.state.name}
+                                                                         location={this.state.location}
+                                                                         menu={this.state.menu}
+              
+              
+              
+              /> : null }
 
 
 
-              {/* { this.props.setCategory === 'cuisine' ? <CuisineItem title={this.props.choiceSet[0].name} image={this.props.choiceSet[0].image}/> : null } */}
               { this.props.setCategory === 'movies' ? <MovieItem poster={this.props.choiceSet[0].poster_path} title={this.props.choiceSet[0].title}/> : null }
 
               { this.props.setCategory === 'cinema' ? <CinemaItem photo={this.props.choiceSet[0].photo_url} 
